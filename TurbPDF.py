19e0eb7d3e510a22baaf664f6468a120adf420e7
Edward Troccoli <et9778@gadi-login-02.gadi.nci.org.au>
2025-03-20 15:08:05 +1100

    Starting Code

--- TurbPDF.py
+++ TurbPDF.py
@@ -12,8 +12,7 @@ from turblib import read_pdf
 from Globals import *
 
 
-
-def plot_pdf(variable,file_name):
+def compute_pdf(variable,file_name):
 
     if not os.path.isdir(fig_path):
         cfp.run_shell_command('mkdir '+fig_path)
@@ -27,25 +26,6 @@ def plot_pdf(variable,file_name):
             vmin = -1e4
             vmax = 1e4
             cfp.run_shell_command(pdfs_mpi file -dset variable -bw 100 -vmin vmin -vmax vmax -log log)
-            ylabel = r'Kinetic Energy'
-        elif variable == "ekdr":
-            log = False
-            vmin = -1e4
-            vmax = 1e4
-            cfp.run_shell_command(pdfs_mpi file -dset variable -bw 100 -vmin vmin -vmax vmax -log log)
-            ylabel = r'Kinetic Energy Dissipation Rate'
-        elif variable == "emdr":
-            log = False
-            vmin = -1e4
-            vmax = 1e4
-            cfp.run_shell_command(pdfs_mpi file -dset variable -bw 100 -vmin vmin -vmax vmax -log log)
-            ylabel = r'Magnetic Energy Dissipation Rate'
-        elif variable == "injr":
-            log = False
-            vmin = -1e4
-            vmax = 1e4
-            cfp.run_shell_command(pdfs_mpi file -dset variable -bw 100 -vmin vmin -vmax vmax -log log)
-            ylabel = r'Injection Rate'
         else:
             print("Variable not implemented.", error=True)
         
@@ -54,6 +34,8 @@ def plot_pdf(variable,file_name):
         #cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
 
     #cfp.plot(xlabel=r'$t/t_\mathrm{turb}$', ylabel=ylabel, save=fig_path+"tevol_"+f"{variable}_"+file_name+'.pdf', legend_loc='best')
+
+def plot_pdf():
 
 
 if __name__ == "__main__":
