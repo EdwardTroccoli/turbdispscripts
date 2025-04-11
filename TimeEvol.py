07285c68deddfe1696dcc5364221a216ebd0634c
Edward Troccoli <et9778@gadi-cpu-clx-2542.gadi.nci.org.au>
2025-04-11 16:41:03 +1000

    removed filename parameter

--- TimeEvol.py
+++ TimeEvol.py
@@ -11,7 +11,7 @@ from cfpack.defaults import *
 from Globals import *
 
 
-def plot_var(variable,file_name):
+def plot_var(variable):
 
     if not os.path.isdir(fig_path):
         cfp.run_shell_command('mkdir '+fig_path)
@@ -55,13 +55,13 @@ def plot_var(variable,file_name):
             injr = dat['#41_injection_rate']
             ekdr = dat['#42_ekin_diss_rate']
             ylabel = r'$\varepsilon_{\textrm{kin}}$ and Injection Rate'
-            cfp.plot(x=time, y=injr, label='Injection Rate (' + path[3:-1] + ')', color=color[i])
-            cfp.plot(x=time, y=ekdr, label='Kinetic Diss. Rate (' + path[3:-1] + ')', color=color[i+1])
+            cfp.plot(x=time, y=injr, label=r'$\varepsilon_{\textrm{inj}}$', color=color[i])
+            cfp.plot(x=time, y=ekdr, label=r'$\varepsilon_{\textrm{kin}}$', color=color[i+1])
         
-    out_path = path + "Figures/"
+    out_path = path + "TimeEvol/"
     if not os.path.isdir(out_path):
         cfp.run_shell_command('mkdir '+out_path)
-    cfp.plot(xlabel=r'$t/t_\mathrm{turb}$', ylabel=ylabel, save=out_path+"tevol_"+f"{variable}_"+file_name+'.pdf', legend_loc='best')
+    cfp.plot(xlabel=r'$t/t_\mathrm{turb}$', ylabel=ylabel, save=out_path+"tevol_"+f"{variable}_.pdf", legend_loc='best')
 
 
 if __name__ == "__main__":
@@ -70,18 +70,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Plot different variables from simulation data.")
     var_choices = ["vstd", "ekin", "emag", "injr", "ekdr", "emdr", "etdr", "ired"]
     parser.add_argument("-v", "--variable", nargs='*', choices=var_choices, required=True, help="Variable to plot; choice of "+str(var_choices))
-    parser.add_argument("-n", "--filename", nargs='*', required=True, help="Give the plotfile a name")
     # Parse the command-line arguments
     args = parser.parse_args()
 
     # Start timing the process
     start_time = timeit.default_timer()
 
-    if len(args.variable) != len(args.filename):
-        raise ValueError("The number of variables and filenames must be the same.")
-
-    for var, file_name in zip(args.variable, args.filename):
-        plot_var(var, file_name)
+    for var in args.variable:
+        plot_var(var)
 
     # End timing and output the total processing time
     stop_time = timeit.default_timer()
