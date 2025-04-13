c34b3755681220f1145aa5533189dcd973d27745
Edward Troccoli <et9778@gadi-login-07.gadi.nci.org.au>
2025-04-13 17:58:33 +1000

    updated to loop over sims outside of plot_var

--- TimeEvol.py
+++ TimeEvol.py
@@ -11,57 +11,53 @@ from cfpack.defaults import *
 from Globals import *
 
 
-def plot_var(variable):
+def plot_var(variable,path):
 
     if not os.path.isdir(fig_path):
         cfp.run_shell_command('mkdir '+fig_path)
 
-    for i, path in enumerate(sim_paths):
-
-        print(f'Working on: {path}', color='green')
-
-        dat = cfp.read_ascii(path+"Turb.dat")
-        time = dat['01_time'] / t_turb[i]
+    dat = cfp.read_ascii(path+"Turb.dat")
+    time = dat['01_time'] / t_turb[i]
         
-        if variable == "vstd":
-            var = dat['#14_rms_velocity']
-            ylabel = r'$\sigma_v$'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "ekin":
-            var = dat['#10_E_kinetic']
-            ylabel = r'$E_\mathrm{kin}$'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "emag":
-            var = dat['#12_E_magnetic']
-            ylabel = r'$E_\mathrm{mag}$'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "injr":
-            var = dat['#41_injection_rate']
-            ylabel = r'injection rate'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "ekdr":
-            var = dat['#42_ekin_diss_rate']
-            ylabel = r'$\varepsilon_{\textrm{kin}}$'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "emdr":
-            var = dat['#43_emag_diss_rate']
-            ylabel = r'magnetic energy dissipation rate'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "etdr":
-            var = dat['#42_ekin_diss_rate'] + dat['#43_emag_diss_rate']
-            ylabel = r'total energy dissipation rate'
-            cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
-        elif variable == "ired":
-            injr = dat['#41_injection_rate']
-            ekdr = dat['#42_ekin_diss_rate']
-            ylabel = r'$\varepsilon_{\textrm{kin}}$ and Injection Rate'
-            cfp.plot(x=time, y=injr, label=r'$\varepsilon_{\textrm{inj}}$', color=color[i])
-            cfp.plot(x=time, y=ekdr, label=r'$\varepsilon_{\textrm{kin}}$', color=color[i+1])
-        
+    if variable == "vstd":
+        var = dat['#14_rms_velocity']
+        ylabel = r'$\sigma_v$'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "ekin":
+        var = dat['#10_E_kinetic']
+        ylabel = r'$E_\mathrm{kin}$'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "emag":
+        var = dat['#12_E_magnetic']
+        ylabel = r'$E_\mathrm{mag}$'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "injr":
+        var = dat['#41_injection_rate']
+        ylabel = r'injection rate'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "ekdr":
+        var = dat['#42_ekin_diss_rate']
+        ylabel = r'$\varepsilon_{\textrm{kin}}$'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "emdr":
+        var = dat['#43_emag_diss_rate']
+        ylabel = r'magnetic energy dissipation rate'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "etdr":
+        var = dat['#42_ekin_diss_rate'] + dat['#43_emag_diss_rate']
+        ylabel = r'total energy dissipation rate'
+        cfp.plot(x=time, y=var, label=path[3:-1], color=color[i])
+    elif variable == "ired":
+        injr = dat['#41_injection_rate']
+        ekdr = dat['#42_ekin_diss_rate']
+        ylabel = r'$\varepsilon_{\textrm{kin}}$ and Injection Rate'
+        cfp.plot(x=time, y=injr, label=r'$\varepsilon_{\textrm{inj}}$', color=color[i])
+        cfp.plot(x=time, y=ekdr, label=r'$\varepsilon_{\textrm{kin}}$', color=color[i+1])
+    
     out_path = path + "TimeEvol/"
     if not os.path.isdir(out_path):
         cfp.run_shell_command('mkdir '+out_path)
-    cfp.plot(xlabel=r'$t/t_\mathrm{turb}$', ylabel=ylabel, save=out_path+"tevol_"+f"{variable}_.pdf", legend_loc='best')
+    cfp.plot(xlabel=r'$t/t_\mathrm{turb}$', ylabel=ylabel, save=out_path+"tevol_"+f"{variable}.pdf", legend_loc='best')
 
 
 if __name__ == "__main__":
@@ -76,8 +72,12 @@ if __name__ == "__main__":
     # Start timing the process
     start_time = timeit.default_timer()
 
-    for var in args.variable:
-        plot_var(var)
+    for i, path in enumerate(sim_paths):
+
+        print(f'Working on: {path}', color='green')
+
+        for var in args.variable:
+            plot_var(var,path)
 
     # End timing and output the total processing time
     stop_time = timeit.default_timer()
