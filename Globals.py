9f99f3ecb5d11168b50e11e6f148316cb6f8b536
Edward Troccoli <et9778@gadi-login-01.gadi.nci.org.au>
2025-04-18 16:22:27 +1000

    Updated for 1024^3

--- Globals.py
+++ Globals.py
@@ -4,8 +4,8 @@
 
 import numpy as np
 
-sim_paths = ["../N512M10HDRe5000/","../N512M0p1HDRe5000/"]
-MachNumber = ["10","0.1"]
-t_turb = np.array([0.05, 5])
-color = ['r','b','g']
+sim_paths = ["../N1024M5HDRe2500/","../N1024M0p2HDRe2500/","../N512M5HDRe2500/","../N512M0p2HDRe2500/","../N256M5HDRe2500/","../N256M0p2HDRe2500/"]
+MachNumber = ["5","0.2", "5","0.2", "5","0.2"]
+t_turb = np.array([0.1, 2.5, 0.1, 2.5, 0.1, 2.5])
+color = ['r','b','g','pink','magenta','yellow']
 fig_path = "../Figures/"
