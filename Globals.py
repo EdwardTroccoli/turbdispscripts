cb23c1a4408a88cba58e5dac493539235daa2853
Edward Troccoli <et9778@gadi-login-03.gadi.nci.org.au>
2025-04-29 20:40:31 +1000

    Updated for new sims

--- Globals.py
+++ Globals.py
@@ -4,8 +4,9 @@
 
 import numpy as np
 
-sim_paths = ["../N1024M5HDRe2500/","../N1024M0p2HDRe2500/","../N512M5HDRe2500/","../N512M0p2HDRe2500/","../N256M5HDRe2500/","../N256M0p2HDRe2500/"]
-MachNumber = ["5","0.2", "5","0.2", "5","0.2"]
-t_turb = np.array([0.1, 2.5, 0.1, 2.5, 0.1, 2.5])
-color = ['r','b','g','pink','magenta','yellow']
+#sim_paths = ["../N1024M5HDRe2500/", "../N1024M0p2HDRe2500/", "../N512M5HDRe2500/", "../N512M0p2HDRe2500/", "../N256M5HDRe2500/", "../N256M0p2HDRe2500/"]
+sim_paths = ["../N1024M0p2HDRe2500/", "../N1024M5HDRe2500/"]
+MachNumber = ["0p2", "5", "5","0p2", "5", "0p2"]
+t_turb = np.array([2.5, 0.1, 0.1, 2.5, 0.1, 2.5])
+color = ['r','b','g','r','b','g','r']
 fig_path = "../Figures/"
