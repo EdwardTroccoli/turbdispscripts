111b031b70da41f04cd2b73290a92c15a214c74c
Edward Troccoli <et9778@gadi-login-03.gadi.nci.org.au>
2025-05-22 15:16:52 +1000

    added mach

--- Globals.py
+++ Globals.py
@@ -5,8 +5,9 @@
 import numpy as np
 
 #sim_paths = ["../N1024M5HDRe2500/", "../N1024M0p2HDRe2500/", "../N512M5HDRe2500/", "../N512M0p2HDRe2500/", "../N256M5HDRe2500/", "../N256M0p2HDRe2500/"]
-sim_paths = ["../N1024M0p2HDRe2500/", "../N1024M5HDRe2500/"]
-MachNumber = ["0p2", "5", "5","0p2", "5", "0p2"]
-t_turb = np.array([2.5, 0.1, 0.1, 2.5, 0.1, 2.5])
-color = ['r','b','g','r','b','g','r']
+sim_paths = ["../N1024M0p2HDRe2500/","../N1024M5HDRe2500/"]#"../N1024M0p2HDRe2500/","../N1024M5HDRe2500/"
+MachNumber = ["0p2", "5", "0p2"]
+Mach = np.array([0.2, 5])
+t_turb = np.array([2.5, 0.1, 0.1, 0.1, 2.5])
+color = ['black','black','black','r','r','g','r']
 fig_path = "../Figures/"
