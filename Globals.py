3fd3ba71f5bbc8dd6d429e9f41c59ec5933f9a94
Christoph Federrath <ccf100@gadi-login-07.gadi.nci.org.au>
2025-05-23 12:16:30 +1000

    cleaning up

--- Globals.py
+++ Globals.py
@@ -5,9 +5,11 @@
 import numpy as np
 
 #sim_paths = ["../N1024M5HDRe2500/", "../N1024M0p2HDRe2500/", "../N512M5HDRe2500/", "../N512M0p2HDRe2500/", "../N256M5HDRe2500/", "../N256M0p2HDRe2500/"]
-sim_paths = ["../N1024M0p2HDRe2500/","../N1024M5HDRe2500/"]#"../N1024M0p2HDRe2500/","../N1024M5HDRe2500/"
-MachNumber = ["0p2", "5", "0p2"]
+
+sim_paths = ["../N1024M0p2HDRe2500/", "../N1024M5HDRe2500/"]
+MachNumber = ["0p2", "5"]
 Mach = np.array([0.2, 5])
+
 t_turb = np.array([2.5, 0.1, 0.1, 0.1, 2.5])
-color = ['black','black','black','r','r','g','r']
+color = ['black', 'black', 'black', 'r', 'r', 'g', 'r']
 fig_path = "../Figures/"
