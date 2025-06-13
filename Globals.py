9e9fb8069e143ca8c9a9eb8dce6c1c203f0d2379
Christoph Federrath <ccf100@gadi-login-09.gadi.nci.org.au>
2025-06-13 19:46:24 +1000

    better way of handling model parameters

--- Globals.py
+++ Globals.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 # written by Edward Troccoli & Christoph Federrath, 2025
 
-import numpy as np
+import cfpack as cfp
+import os
 
-#sim_paths = ["../N1024M5HDRe2500/", "../N1024M0p2HDRe2500/", "../N512M5HDRe2500/", "../N512M0p2HDRe2500/", "../N256M5HDRe2500/", "../N256M0p2HDRe2500/"]
-
-sim_paths = ["../N1024M0p2HDRe2500/", "../N1024M5HDRe2500/"]
-MachNumber = ["0p2", "5"]
-Mach = np.array([0.2, 5])
-
-t_turb = np.array([2.5, 0.1, 0.1, 0.1, 2.5])
-color = ['black', 'black', 'black', 'r', 'r', 'g', 'r']
+# create figure output path
 fig_path = "../Figures/"
+if not os.path.isdir(fig_path):
+    cfp.run_shell_command('mkdir '+fig_path)
+
+# sim_paths = ["../N1024M0p2HDRe2500HP/", "../N1024M0p2HDRe2500/", "../N1024M5HDRe2500HP/", "../N1024M5HDRe2500/"]
+sim_paths = ["../N1024M0p2HDRe2500/", "../N1024M5HDRe2500/"]
+
+def params(model_name):
+    class ret:
+        if 'M0p2' in model_name: Mach = 0.2
+        if 'M5' in model_name: Mach = 5
+        t_turb = 0.5 / Mach
+        # do the color selection here as well, based on the model name / path; needs implementation...
+        color = 'black'
+    return ret
