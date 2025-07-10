9fd431f1b68a6543d08b15dee6b6f0e4cc70dd63
Christoph Federrath <lu79haha@login04.sng.lrz.de>
2025-07-10 09:24:22 +0200

    spectra

--- Globals.py
+++ Globals.py
@@ -201,7 +201,7 @@ def compute_spectra_file(filename, out_path='./', ncpu=8, overwrite=False):
 def compute_spectra(overwrite=False):
     for sim_path in sim_paths:
         if params(sim_path).N == 2048:
-            ncpu = 2048
+            ncpu = 512
         elif params(sim_path).N == 1024:
             ncpu = 64
         else:
