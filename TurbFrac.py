cf9bbff49c5d62d215c295e72daf0d9ca8ffdaeb
Christoph Federrath <ccf100@gadi-login-03.gadi.nci.org.au>
2025-08-01 12:09:10 +1000

    plotting

--- TurbFrac.py
+++ TurbFrac.py
@@ -12,43 +12,17 @@ import matplotlib.pyplot as plt
 import flashlib as fl
 from scipy.stats import linregress
 
-# def compute_ekdr_size_fractal_dim(filename, lmax=1.0):
-#     gg = fl.FlashGG(filename)
-#     centre = gg.GetMaxLoc("ekdr")
-#     N = gg.NMax[0]
-#     D = gg.D[0][0]
-#     sum = []
-#     size = []
-#     for l in range(1, N, 2):
-#         print("Working on size l = "+str(l*D), color="cyan")
-#         box_bounds = np.array([centre-l*D/2, centre+l*D/2]).T
-#         co = gg.GetCells("ekdr", box_bounds=box_bounds)
-#         sum.append(co.cell_dat.sum())
-#         size.append(l*D)
-#     sum = np.log(np.array(sum))
-#     size = np.log(np.array(size))
-#     slope, intercept, r_value, p_value, std_err = linregress(size, sum)
-#     stop()
-#     return slope
-
-def plot_ekdr_size_fractal_dim(outfile, bsdat):
-    stop()
-    cfp.plot(x = bsdat.x, y = bsdat.y, show = True)
-    slope, intercept, r_value, p_value, std_err = linregress(bsdat.x, bsdat.y)
 
 if __name__ == "__main__":
 
     # Argument parser setup
-    parser = argparse.ArgumentParser(description="Compute fractal dimension from FLASH simulation data.")
-    parser.add_argument("-ov", "--overwrite", action='store_true', default=False, help="Overwrite files")
-
+    parser = argparse.ArgumentParser(description="Plot fractal dimension.")
     args = parser.parse_args()
-
-    # Start timing the process
-    start_time = timeit.default_timer()
 
     # loop over simulations
     for i, path in enumerate(sim_paths):
+
+        print(f'\nWorking on: {path}', color='cyan')
 
         N = params(path).N
         t_turb = params(path).t_turb
@@ -56,19 +30,20 @@ if __name__ == "__main__":
         if Mach == 0.2: MachStr = '0p2'
         if Mach == 5:   MachStr = '5'
 
-        print(f'\nWorking on: {path}', color='cyan')
 
-        # creates the file output dir
-        out_path = path + "FracDim/"
-        if not os.path.isdir(out_path):
-            cfpshe.run_ll_command('mkdir '+out_path)
-
-        # Read pkl file data
+        # Read data and plot
         bsdat = get_ekdr_size_fractal_dim(path)
+        outfile = path+'FracDim/aver_ekdr_vs_size'+MachStr+'.pdf'
+        ylabel = r"Dissipation rate $\varepsilon_{\textrm{kin}}/(\langle\rho\rangle\,\mathcal{M}^2\, c_{\textrm{s}}^2\,t_{\textrm{turb}}^{-1}$)"
 
-        # Plot ekdr_size_fractal_dim
-        outfile = out_path+'aver_ekdr_vs_size'+MachStr+'.pdf'
-        
-        # Plot ekdr_size_fractal_dim
-        plot_ekdr_size_fractal_dim(outfile, bsdat)
+        cfp.plot(x=bsdat.x, y=bsdat.y, type='scatter', label='sim')
+        cfp.plot(x=bsdat.x, y=1e2*bsdat.x**2, label=r'$\propto r^2$')
+        cfp.plot(x=bsdat.x, y=1e2*bsdat.x**3, label=r'$\propto r^3$')
+        cfp.plot(xlog=True, ylog=True, xlabel=r"Distance $r$", ylabel=ylabel, show=True)
+
+        stop()
+
+        # fit
+        slope, intercept, r_value, p_value, std_err=linregress(bsdat.x, bsdat.y)
+
         print(f"Estimated fractal dimension: {np.mean(frac_dims):.3f}")
